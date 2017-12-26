db2cb6022496056c499b15e4642157240b0aac26
Alban VIDAL <alban.vidal@zordhak.fr>
2017-12-26 18:20:22 +0100

    strip debug symbol from binary files; Add upstream changelog

--- 02_create_deb_dnsperf.sh
+++ 02_create_deb_dnsperf.sh
@@ -55,23 +55,27 @@ The Debian packaging is
     Copyright (C) 2017 Alban VIDAL <alban.vidal@zordhak.fr>.
 EOF
 
-# Changelog file
-cat << EOF > ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog
+# Changelog file (Debian)
+cat << EOF > ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog.Debian
 dnsperf (2.1.1.0.d-1) stretch; urgency=low
 
   * Initial Release of Debian package for dnsperf
 
  -- Alban Vidal <alban.vidal@zordhak.fr>  $(LANG=en_US date "+%a, %d %b %Y %R:%S %z")
 EOF
-# compress changelog file
-gzip --best --no-name ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog
+# compress changelog file (Debian)
+gzip --best --no-name ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog.Debian
 
 ################################################################################
 # Copie des fichiers
 cd $SOURCE_DIR
-/usr/bin/install -c  dnsperf ${BASE_DIR}/$VERSION/usr/bin
-/usr/bin/install -c  resperf ${BASE_DIR}/$VERSION/usr/bin
-/usr/bin/install -c  resperf-report ${BASE_DIR}/$VERSION/usr/bin
+/usr/bin/install -s dnsperf ${BASE_DIR}/$VERSION/usr/bin
+# also possible:
+#objcopy --strip-debug --strip-unneeded dnsperf ${BASE_DIR}/$VERSION/usr/bin/dnsperf
+/usr/bin/install -s resperf ${BASE_DIR}/$VERSION/usr/bin
+# also possible:
+#objcopy --strip-debug --strip-unneeded resperf ${BASE_DIR}/$VERSION/usr/bin/resperf
+/usr/bin/install -c resperf-report ${BASE_DIR}/$VERSION/usr/bin
 /usr/bin/install -c -m 644  dnsperf.1 ${BASE_DIR}/$VERSION/usr/share/man/man1
 /usr/bin/install -c -m 644  resperf.1 ${BASE_DIR}/$VERSION/usr/share/man/man1
 # Create symbolic link for resperf-report to resperf
@@ -81,6 +85,9 @@ ln -s resperf.1.gz ${BASE_DIR}/$VERSION/usr/share/man/man1/resperf-report.1.gz
 # --no-name => to supress package-contains-timestamped-gzip
 gzip --best --no-name ${BASE_DIR}/$VERSION/usr/share/man/man1/dnsperf.1
 gzip --best --no-name ${BASE_DIR}/$VERSION/usr/share/man/man1/resperf.1
+# Original changelog
+cp RELEASE_NOTES ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog
+gzip --best --no-name ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog
 
 # Generate md5sum for each file
 cd ${BASE_DIR}/$VERSION/
