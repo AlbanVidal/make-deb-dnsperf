3c785d74276525423d43fad6aef9068cc5ae0930
Alban VIDAL <alban.vidal@zordhak.fr>
2017-12-28 17:31:08 +0100

    Move DEBIAN/control, changelog.Debian and copyright to a specific files

--- 02_create_deb_dnsperf.sh
+++ 02_create_deb_dnsperf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TEMPLATE_DIR="/srv/git/make-deb-dnsperf"
+
 BASE_DIR="/opt/deb"
 SOURCE_DIR="/opt/dnsperf-src/dnsperf"
 
@@ -8,11 +10,13 @@ version_majeur=$(grep 'VERSION "' ${SOURCE_DIR}/version.h|awk '{print $3}'|sed '
 
 # année mois jour heure
 #version_mineur=$(date +%Y%m%d.%H)
-version_mineur=1
+#version_mineur=1
+version_deb=$(cat ${TEMPLATE_DIR}/VERSION_DEB)
 #
 version_arch=$(dpkg --print-architecture)
 #
-VERSION="dnsperf-${version_majeur}-${version_mineur}_${version_arch}"
+#VERSION="dnsperf-${version_majeur}-${version_mineur}_${version_arch}"
+VERSION="dnsperf-${version_majeur}-${version_deb}_${version_arch}"
 
 # Si le répertoire existe déjà on supprime
 # If directory exist, will be delete it
@@ -31,38 +35,44 @@ mkdir -p ${BASE_DIR}/$VERSION/usr/share/man/man1/
 # doc dir
 mkdir -p ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf
 
-# Copyright file
-cat << EOF > ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/copyright
-This is the Debian GNU/Linux prepackaged version of dnsperf
+## Copyright file
+cp ${TEMPLATE_DIR}/TEMPLATE_usr_share_doc_dnsperf_copyright ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/copyright
+## OLD A RM :
+#cat << EOF > ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/copyright
+#This is the Debian GNU/Linux prepackaged version of dnsperf
+#
+#dnsperf - DNS Performance Testing Tools - Apache License Version 2.0
+#
+#This package is maintained for Debian by Alban VIDAL <alban.vidal@zordhak.fr>,
+#and was built from the sources found at:
+#
+#    https://github.com/nominum/dnsperf
+#
+#Official website of nominum:
+#
+#    https://www.nominum.com/
+#    https://www.nominum.com/measurement-tools/
+#
+#On Debian GNU/Linux systems, the complete text of the Apache License
+#Version 2.0 can be found in /usr/share/common-licenses/Apache-2.0.
+#
+#The Debian packaging is
+#
+#    Copyright (C) 2017 Alban VIDAL <alban.vidal@zordhak.fr>.
+#EOF
 
-dnsperf - DNS Performance Testing Tools - Apache License Version 2.0
-
-This package is maintained for Debian by Alban VIDAL <alban.vidal@zordhak.fr>,
-and was built from the sources found at:
-
-    https://github.com/nominum/dnsperf
-
-Official website of nominum:
-
-    https://www.nominum.com/
-    https://www.nominum.com/measurement-tools/
-
-On Debian GNU/Linux systems, the complete text of the Apache License
-Version 2.0 can be found in /usr/share/common-licenses/Apache-2.0.
-
-The Debian packaging is
-
-    Copyright (C) 2017 Alban VIDAL <alban.vidal@zordhak.fr>.
-EOF
 
 # Changelog file (Debian)
-cat << EOF > ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog.Debian
-dnsperf (2.1.1.0.d-1) stretch; urgency=low
-
-  * Initial Release of Debian package for dnsperf
-
- -- Alban Vidal <alban.vidal@zordhak.fr>  $(LANG=en_US date "+%a, %d %b %Y %R:%S %z")
-EOF
+cp ${TEMPLATE_DIR}/TEMPLATE_usr_share_doc_dnsperf_changelog.Debian ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog.Debian
+# To generate date field: LANG=en_US date "+%a, %d %b %Y %R:%S %z"
+## A RM
+#cat << EOF > ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog.Debian
+#dnsperf (2.1.1.0.d-1) stretch; urgency=low
+#
+#  * Initial Release of Debian package for dnsperf
+#
+# -- Alban Vidal <alban.vidal@zordhak.fr>  $(LANG=en_US date "+%a, %d %b %Y %R:%S %z")
+#EOF
 # compress changelog file (Debian)
 gzip --best --no-name ${BASE_DIR}/$VERSION/usr/share/doc/dnsperf/changelog.Debian
 
@@ -99,25 +109,32 @@ installedSize=$(( totalSize / 1024 ))
 (( installedSize ++ ))
 
 # Debian control
-cat << EOF > ${BASE_DIR}/$VERSION/DEBIAN/control
-Package: dnsperf
-Version: $version_majeur-$version_mineur
-Section: admin
-Priority: optional
-Architecture: $version_arch
-Installed-Size: $installedSize
-Depends: libbind9-140, libssl1.1, libc6
-Maintainer: Alban Vidal <alban.vidal@zordhak.fr>
-Homepage: https://github.com/nominum/dnsperf
-Description: DNS Performance Testing Tools
- This is a collection of DNS server performance testing tools, including
- dnsperf and resperf. For more information, see the dnsperf(1) and resperf(1)
- man pages.
- .
- Usage:
- dnsperf and resperf read input files describing DNS queries, and send those
- queries to DNS servers to measure performance.
-EOF
+# Copie file
+cp ${TEMPLATE_DIR}/TEMPLATE_DEBIAN_control ${BASE_DIR}/$VERSION/DEBIAN/control
+# Edit field
+sed -i "s/%%VERSION%%/$version_majeur-$version_deb/"    ${BASE_DIR}/$VERSION/DEBIAN/control
+sed -i "s/%%ARCH%%/$version_arch/"                      ${BASE_DIR}/$VERSION/DEBIAN/control
+sed -i "s/%%INSTALLED_SIZE%%/$installedSize/"           ${BASE_DIR}/$VERSION/DEBIAN/control
+## OLD A RM
+#cat << EOF > ${BASE_DIR}/$VERSION/DEBIAN/control
+#Package: dnsperf
+#Version: $version_majeur-$version_deb
+#Section: admin
+#Priority: optional
+#Architecture: $version_arch
+#Installed-Size: $installedSize
+#Depends: libbind9-140, libssl1.1, libc6
+#Maintainer: Alban Vidal <alban.vidal@zordhak.fr>
+#Homepage: https://github.com/nominum/dnsperf
+#Description: DNS Performance Testing Tools
+# This is a collection of DNS server performance testing tools, including
+# dnsperf and resperf. For more information, see the dnsperf(1) and resperf(1)
+# man pages.
+# .
+# Usage:
+# dnsperf and resperf read input files describing DNS queries, and send those
+# queries to DNS servers to measure performance.
+#EOF
 
 
 
